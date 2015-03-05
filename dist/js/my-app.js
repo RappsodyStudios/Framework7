77f3be078c270880e7fb4b6f9bcf8763a8dba164
Joshua Rapp <joshua.rapp@phishme.com>
2015-03-05 00:51:34 -0500

    Added Data Charting
    Added Charts.js to implement all of the data visualization throughout
    the application from the item purchase history to the mobile analytics
    screen.

--- dist/js/my-app.js
+++ dist/js/my-app.js
@@ -43,3 +43,85 @@ $$(".checkbox").click(function(){
 		$$("#item").show();
 	}
 });
+
+//Particular function for the item details screen
+myApp.onPageInit('details', function(page){
+	
+	var randomScalingFactor = function(){ return Math.round(Math.random()*12)};
+    var purchaseChartData = {
+        labels : ["Oct", "Nov"],
+        datasets : [
+            {
+                label: "Activity for: ",
+                fillColor : "rgba(250,221,153,0.2)",
+                strokeColor : "rgba(224,143,80,1)",
+                pointColor : "rgba(220,220,220,1)",
+                pointStrokeColor : "#e08f50",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(220,220,220,1)",
+                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+            }
+        ]
+    }
+
+    var ctxPurchase = document.getElementById("purchase_chart").getContext("2d");
+    window.myLine = new Chart(ctxPurchase).Line(purchaseChartData, {responsive: true});
+	
+});
+
+//Initializes the charts on the analytics page
+myApp.onPageInit('anayltics', function(page){
+	
+	var doughnutData = [
+				{
+					value: 300,
+					color:"#6aa9d4"
+				},
+				{
+					value: 50,
+					color: "#46BFBD"
+				},
+				{
+					value: 100,
+					color: "#f1e786"
+				},
+				{
+					value: 40,
+					color: "#e08f50"
+				},
+				{
+					value: 120,
+					color: "#dc5655"
+				}
+
+			];
+			
+
+    var randomScalingFactor = function(){ return Math.round(Math.random()*12)};
+    var lineChartData = {
+        labels : ["8am","10am","12pm","2pm","4pm","6pm","8pm"],
+        datasets : [
+            {
+                label: "Activity for: ",
+                fillColor : "rgba(250,221,153,0.2)",
+                strokeColor : "rgba(224,143,80,1)",
+                pointColor : "rgba(220,220,220,1)",
+                pointStrokeColor : "#e08f50",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(220,220,220,1)",
+                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+            }
+        ]
+    }
+
+    var ctxLine = document.getElementById("line_chart").getContext("2d");
+    window.myLine = new Chart(ctxLine).Line(lineChartData, {responsive: true});
+	
+	var ctxDoughnut = document.getElementById("chart_area").getContext("2d");
+	window.myDoughnut = new Chart(ctxDoughnut).Doughnut(doughnutData, {responsive : true});
+});
+
+//Particular function for the recipes screen
+myApp.onPageInit('recipes', function(page){
+	
+});
